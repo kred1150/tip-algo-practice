916327e3436538cc9de392e876d1290ccdfc7a40
Kevin Redmond <kred1150@gmail.com>
2022-07-25 20:56:46 -0500

    add map_double

--- array_max.rb
+++ array_max.rb
@@ -2,3 +2,19 @@
 
 # Input: [5, 17, -4, 20, 12]
 # Output: 20
+
+array = [5, 17, -4, 20, 12]
+
+def max(array)
+  index = 0
+  max = array[0]
+  while index < array.length
+    if max < array[index]
+      max = array[index]
+    end
+    index += 1
+  end
+  return max
+end
+
+p max(array)
